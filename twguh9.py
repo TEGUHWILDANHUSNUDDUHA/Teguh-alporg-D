99ac0bb17b6328908becc8adfb8f23b6a4fe74e6
TEGUHWILDANHUSNUDDUHA <teguhwildan435@gmail.com>
2024-12-15 12:14:34 +0800

    Add files via upload

---
+++ twguh9.py
@@ -0,0 +1,17 @@
+# suhu dalam celcius = 29
+# suhu dalam fahrenheit = 84.2
+# suhu dalam kelvin = 302.15
+# suhu dalam reamur = 23.2
+
+# suhu dalam celcius
+celcius = 29
+
+# rumus koversi suhu
+farenheit = (celcius * 9/5) + 32
+kelvin = celcius + 273.15
+reamur = celcius * 4/5
+
+# cetak nilai suhu
+print("suhu dalam fahrenheit:", farenheit )
+print("suhu dalam kelvin:", kelvin)
+print("suhu dalam reamur:", reamur)
